b471768457ba7b20a2a83f92235dbffa00a63cd7
Asaan12 <mathewasaan@gmail.com>
2023-11-16 16:54:08 -0800

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,19 +7,16 @@
  *
  * Return: the number of nodes printed
  */
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-size_t s = 0;
+size_t num = 0;
 
 while (h)
 {
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
+printf("%d\n", h->n);
+num++;
 h = h->next;
-s++;
 }
 
-return (s);
+return (num);
 }
