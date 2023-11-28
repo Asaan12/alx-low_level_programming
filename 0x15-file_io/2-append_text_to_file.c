3f933e9b2dc94f6dfe4e76d0528e13441ae9a139
Asaan12 <mathewasaan@gmail.com>
2023-11-28 12:03:12 -0800

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int alphabets;
+	int nletters;
 	int rwr;
 
 	if (!filename)
@@ -24,10 +24,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (alphabets = 0; text_content[alphabets]; alphabets++)
+		for (nletters = 0; text_content[nletters]; nletters++)
 			;
 
-		rwr = write(fd, text_content, alphabets);
+		rwr = write(fd, text_content, nletters);
 
 		if (rwr == -1)
 			return (-1);
